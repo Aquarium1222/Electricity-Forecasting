382a208f8820674628452e58d771ba8da447b1fd
gougon <freddy2531@gmail.com>
2022-03-26 17:04:10 +0800

    Feat: Add ouput file format

--- config.py
+++ config.py
@@ -1,3 +1,14 @@
 class Constant:
     SUPPLY_DEMAND_ELECTRICITY_FILE = './data/台灣電力公司_過去電力供需資訊.csv'
     RESERVE_MARGIN = './data/本年度每日尖峰備轉容量率.csv'
+    OUTPUT_FILE = './submission.csv'
+    # Format
+    #
+    # date, operating_reserve(MW)
+    # 20210323, 2557
+    # 20210324, 1899
+    # 20210325, 1891
+    # 20210326, 1811
+    # 20210327, 1903
+    # 20210328, 2333
+    # 20210329, 1800
