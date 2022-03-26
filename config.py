ff0e98ae5c82ed851e4e505a0c99f1b6d8c0004f
gougon <freddy2531@gmail.com>
2022-03-26 23:19:27 +0800

    Feat: Use simple linear regression

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Constant:
-    SUPPLY_DEMAND_ELECTRICITY_FILE = './data/台灣電力公司_過去電力供需資訊.csv'
-    RESERVE_MARGIN = './data/本年度每日尖峰備轉容量率.csv'
+    SUPPLY_DEMAND_ELECTRICITY_FILES = ['./data/台灣電力公司_過去電力供需資訊_1.csv', './data/台灣電力公司_過去電力供需資訊_2.csv']
+    RESERVE_MARGIN = ['./data/每日尖峰備轉容量率_1.csv', './data/每日尖峰備轉容量率_2.csv']
     OUTPUT_FILE = './submission.csv'
     # Format
     #
